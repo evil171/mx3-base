bde88d046cf3b8f1b88f6f606d41061ac9b21aa4
雨E <evilbai@foxmail.com>
2022-12-03 19:18:24 +0800

    Init

--- src/main/java/com/example/mx3/bean/CaptchaBean.java
+++ src/main/java/com/example/mx3/bean/CaptchaBean.java
@@ -4,13 +4,20 @@ import lombok.Data;
 import lombok.experimental.Accessors;
 
 /**
- *
  * @since 2020/3/7 10:35 下午
  */
 @Data
 @Accessors(chain = true)
 public class CaptchaBean {
 
+    /**
+     * 图片
+     */
     private String base64;
+
+    /**
+     * token
+     */
+    private String token;
 
 }
